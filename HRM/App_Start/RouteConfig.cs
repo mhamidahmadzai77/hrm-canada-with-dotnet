6c7ffe51352f1b69b3244eb4c752cb1bda925a52
humanreliefmission-160 <media@humanreliefmission.com>
2026-01-06 13:29:00 +0430

    App

---
+++ HRM/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HRM
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "appeal",
+                url: "{controller}/{action}/{appeal}/{id}",
+                defaults: new { controller = "Home", action = "Index", appeal = UrlParameter.Optional, id = UrlParameter.Optional }
+            );
+
+           
+
+        }
+    }
+}
